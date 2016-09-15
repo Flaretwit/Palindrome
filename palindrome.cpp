1365d30f59cbc0268c113d530bbd21be96e80695
Ke Shen <296697@beaverton.k12.or.us>
2016-09-15 14:18:17 -0700

    Finished code. Commented.

--- palindrome.cpp
+++ palindrome.cpp
@@ -2,24 +2,47 @@
 #include<string.h>
 #include<iostream>
 #include<stdlib.h>
-#include<array>
+#include <cctype>
+#include <cstring>
 
+using namespace std;
 main() {
-	array<char, 81> input;
-
-	cout << "Enter in your input";
-
+	char input[81];
+	bool isValid = true;
+	int len = 0;
+	cout << "Enter in your input: \n";
 	cin >> input;
-
-	for(int i = 0; i < input.size(); i++) {
-		if(!isalpha(input[i])) {
+	len = strlen(input);
+	//Discards punctuation
+	for(int i = 0; i < len; i++) {
+		if(!isalpha(input[i]) && !isdigit(input[i])) { 
 			//if a character is found to not be alphanumeric, then it overwrites
 			//it by copying over the remaining part of the array
-			for(int j = i+1; j < input.size(); j++) {
-				input[j-1] = input[j];
-				cout << "working";
+			for(int j = i+1; j < len; j++) {
+				//each element in the array is replaced by the one to its right, starting with the
+				//index whose element was not alphanumeric.
+				input[j-1] = input[j];				
 			}
+			//reduces the variable length by one to avoid checking it (just ditching it for now).
+			len = len -1;
 		}
 	}
+	//now runs through the array comparing the very front to the very back
+	//and moving inwards. Sets a variable to false to indicate the input is not a palindrome
+	for(int i = 0; i < len; i++) {
+		if(input[i] != input[len-1 - i]) {
+			isValid = false;
+		}
+	}
+	//outputs
+	if(isValid) {
+		cout << "Palindrome";
+	}
+
+	else {
+		cout << "Not a Palindrome";
+	}
+
+
 }
 
